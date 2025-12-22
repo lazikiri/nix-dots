d323369f65335ca9c991291fe4ea51791537e883
lazikiri <lazikiri@tuta.com>
2025-12-22 04:42:46 -0500

    Redisabled home-manager, for now

--- flake.nix
+++ flake.nix
@@ -24,17 +24,17 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./nixos/hosts/laptop/configuration.nix
         nur.modules.nixos.default
         nur.legacyPackages."x86_64-linux".repos.iopq.modules.xraya
         home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.user = import ./home.nix;
-          };
-        }
+        # {
+        #   home-manager = {
+        #     useGlobalPkgs = true;
+        #     useUserPackages = true;
+        #     users.user = import ./home.nix;
+        #   };
+        # }
         nix-flatpak.nixosModules.nix-flatpak
       ];
     };
