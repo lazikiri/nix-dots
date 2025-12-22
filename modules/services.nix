b33a0df234b26b0181af349a97ef7f02575378d7
lazikiri <lazikiri@tuta.com>
2025-12-22 00:35:02 -0500

    Made services.nix a bit more organized

--- modules/services.nix
+++ modules/services.nix
@@ -3,43 +3,45 @@
   pkgs,
   ...
 }: {
-  services.auto-cpufreq.enable = true;
+  services = {
+    auto-cpufreq.enable = true;
 
-  services.flatpak = {
-    enable = true;
-    update.onActivation = true;
-    remotes = [
-      {
-        name = "flathub-beta";
-        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-      }
-      {
-        name = "flathub";
-        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      }
-    ];
-    packages = [
-      "com.dec05eba.gpu_screen_recorder"
-      "com.discordapp.Discord"
-      "com.github.tchx84.Flatseal"
-      "com.usebottles.bottles"
-      "io.github.flattool.Warehouse"
-      "io.github.lawstorant.boxflat"
-      "org.vinegarhq.Sober"
-    ];
+    flatpak = {
+      enable = true;
+      update.onActivation = true;
+      remotes = [
+        {
+          name = "flathub-beta";
+          location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+        }
+        {
+          name = "flathub";
+          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+        }
+      ];
+      packages = [
+        "com.dec05eba.gpu_screen_recorder"
+        "com.discordapp.Discord"
+        "com.github.tchx84.Flatseal"
+        "com.usebottles.bottles"
+        "io.github.flattool.Warehouse"
+        "io.github.lawstorant.boxflat"
+        "org.vinegarhq.Sober"
+      ];
+    };
+
+    getty.autologinUser = "user";
+
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    printing.enable = false;
   };
-
-  services.getty.autologinUser = "user";
-
-  services.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  services.printing.enable = false;
 
   xdg.portal = {
     enable = true;
