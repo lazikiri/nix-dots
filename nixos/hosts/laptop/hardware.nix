100df2c09ed194c9b2f0998b8dd425dd257a8c82
lazikiri <lazikiri@tuta.com>
2025-12-23 04:49:31 -0500

    Driver updates

--- nixos/hosts/laptop/hardware.nix
+++ nixos/hosts/laptop/hardware.nix
@@ -6,15 +6,45 @@
 }: {
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
   };
 
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = ["modesetting"];
 
   hardware.nvidia = {
+    # Modesetting is required.
     modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+    # Only available from driver 515.43.04+
     open = true;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
     nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.nvidia.prime = {
+    sync.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
   };
 
   hardware.openrazer.enable = true;
@@ -28,6 +58,8 @@
     extraModulePackages = [config.boot.kernelPackages.msi-ec];
     kernelParams = [
       "ec_sys.write_support=1"
+  "nvidia-drm.modeset=1"
+  "nvidia-drm.fbdev=1"
     ];
     kernelModules = [
       "ec_sys"
