a2c82d5f18164b7a9aff6a33a633653653d87d98
lazikiri <lazikiri@tuta.com>
2026-01-03 00:29:51 -0500

    Removed user packages

--- nixos/users/users.nix
+++ nixos/users/users.nix
@@ -4,33 +4,5 @@
     description = "User";
     extraGroups = ["networkmanager" "wheel" "openrazer" "plugdev"];
     shell = pkgs.fish;
-
-    packages = with pkgs; [
-      alejandra
-      brave
-      btop
-      lazygit
-      ncdu
-      nwg-look
-      pkgs.nur.repos.forkprince.helium-nightly
-      unp
-      unzip
-      stow
-      (discord.override {
-        withVencord = true;
-        withOpenASAR = true;
-      })
-      easyeffects
-
-      # Neovim
-      neovim
-      ripgrep
-
-      mpv
-
-      tmux
-
-      nautilus
-    ];
   };
 }
