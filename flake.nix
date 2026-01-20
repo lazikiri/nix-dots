4ba81044b9305c802f99a9dea36fde82d84c3e96
lazikiri <lazikiri@tuta.com>
2026-01-20 01:12:58 -0500

    added nixpkgs-25.05 to flake, removed xlibre comments

--- flake.nix
+++ flake.nix
@@ -3,33 +3,31 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-25_05.url = "nixpkgs/nixos-25.05";
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    # xlibre-overlay = {
-    #   url = "git+https://codeberg.org/takagemacoed/xlibre-overlay";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
   outputs = inputs @ {
     nixpkgs,
+    nixpkgs-25_05,
     nur,
     nix-flatpak,
-    # xlibre-overlay,
     ...
   }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { 
+        stable-pkgs = nixpkgs-25_05.legacyPackages.x86_64-linux; 
+      };
       modules = [
         ./nixos/hosts/laptop/configuration.nix
         nur.modules.nixos.default
         nur.legacyPackages."x86_64-linux".repos.iopq.modules.xraya
         nix-flatpak.nixosModules.nix-flatpak
-        # inputs.xlibre-overlay.nixosModules.overlay-xlibre-xserver
-        # inputs.xlibre-overlay.nixosModules.overlay-all-xlibre-drivers
       ];
     };
   };
